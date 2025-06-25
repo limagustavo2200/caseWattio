7cc0db3a4b87c93d4d4f01d87857ae219c6e70fe
limagustavo2200 <limagustavo2200@gmail.com>
2025-06-24 23:13:20 -0300

    Esvaziando main

--- src/adapters/driver/api/routes.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-# Rotas FastAPI para filmes
-router = APIRouter()
-
-@router.get("/")
-def read_root():
-    return {"Hello": "World"}
--- src/app.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-from adapters.driver.api.routes import router
-
-app = FastAPI()
-app.include_router(router)
